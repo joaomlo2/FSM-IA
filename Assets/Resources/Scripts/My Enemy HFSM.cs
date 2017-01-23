f6473e1e9d4551869110ef4672138b9037f3ec63
joaomlo <joaomlo2@gmail.com>
2017-01-23 15:58:31 +0000

    Primeira versão da minha (H?)SM. Faz mesmo muita falta poder testar isto, mas penso que terá de ser para outra altura.

--- Assets/Resources/Scripts/My Enemy HFSM.cs
+++ Assets/Resources/Scripts/My Enemy HFSM.cs
@@ -23,6 +23,7 @@ public interface ICondition
 public class Transition : ITransition
 {
     public int TransitionLevel;
+    public List<IAction> Actions;
     public List<ICondition> ConditionsList;
     public State targetState;
 
@@ -41,6 +42,7 @@ public class State
 {
     protected string StateName;
     public int StateLevel;
+    public State ParentState;
     public List<IAction> Actions;
     public IAction EntryAction;
     public IAction ExitAction;
@@ -69,33 +71,54 @@ public class HSMController
 {
     public State initialState;//May be important to keep in mind which is the main HSM
     public State currentState;//You have to know where you left off
-
-    public List<IAction> update()
-    {
-
-    }
 }
 
 public class HSM:State
 {
     public string HSMName;
-    public HSM parent;
-    public int HSMLevel; //0 means main HSM Level
-    public List<State> states; //States or other sub-HSM's
-    public State initialState;
-    public State currentState;//You have to know where you left off
+    public int HSMLevel; //0 significa o nível principal da HSM
+    public List<State> states; //Estados ou outras sub-HSMs
+    public State initialState;//Para saber o estado inicial da máquina
+    public State currentState;//Para saber em que estado é que esta máquina se encontra
 
     public List<IAction> update()
     {
         if (currentState == null)
         {
             currentState = initialState;
-            if (currentState is HSM)
+        }
+        foreach (Transition transition in transitions)
+        {
+            //Se todas as condições de transição se verificarem
+            if (transition.CanPerformTransition())
             {
-                HSM returner = currentState as HSM;
-                List<IAction> a;
-                return returner.update();
+                List<IAction> actionsToReturn = new List<IAction>();
+                //Serão adicionadas à lista de retorno as acções de saída do estado actual, as acções da transição e as acções de entrada do estado alvo da transição
+                actionsToReturn.Add(currentState.ExitAction);
+                actionsToReturn.AddRange(transition.Actions);
+                actionsToReturn.Add(transition.targetState.EntryAction);
+                //O estado actual passa a ser o que foi alvo da transição
+                currentState = transition.targetState;
+                //Retorna-se a lista de acções
+                return actionsToReturn;
             }
+        }
+        if (currentState is HSM)
+        {
+            HSM returner = currentState as HSM;
+            //Cria-se a lista que irá ser retornada por este método
+            List<IAction> actionsToReturn=new List<IAction>();
+            //Adicionam-se as próprias acções
+            actionsToReturn.AddRange(Actions);
+            //Sendo que isto não á apenas um estado, mas uma HSM que contem outros estados, adicionam-se também as acções dos estados actuais dentro dela
+            actionsToReturn.AddRange(returner.update());
+            //Retorna-se a lista com todas as acções
+            return actionsToReturn;
+        }
+        else
+        {
+            //Caso seja apenas um estado simples (e tendo em conta que não foram accionadas transições), apenas se irão retornar as acções respectivas
+            return currentState.Actions;
         }
     }
 }
